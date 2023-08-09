4c94e3d79593ca7bb53c01292b46b47b7df6f718
Baljot Hansi <baljotshansi@gmail.com>
2023-08-09 16:05:40 -0300

    Added logic to randomly generate password based on which checkboxes are checked. Need to iron out some related bugs.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,17 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
-var checkboxLength = document.querySelector("#checkbox-length");
 var inputLengthSet = document.querySelector("#length-set");
+let inputLength = document.querySelector("#input-length");
+
+// All checkboxes
+let checkboxLength = document.querySelector("#checkbox-length");
+let checkboxLowercase = document.querySelector("#checkbox-lowercase");
+let checkboxUppercase = document.querySelector("#checkbox-uppercase");
+let checkboxNumeric = document.querySelector("#checkbox-numeric");
+let checkboxSpecial = document.querySelector("#checkbox-special");
+
+const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const specialCharacters = "~`!@#$%^&*()_-+={[}]|\\:;\"'<,>.?/";
 
 var passwordLength = 8;
 
@@ -9,6 +19,8 @@ function criteriaLength() {
   let lengthInputDiv = document.querySelector("#length-input-div");
   if(checkboxLength.checked) {
     lengthInputDiv.style.display = "block";
+    if(!checkboxUppercase.checked)
+      checkboxLowercase.checked = true;
   }
   else {
     lengthInputDiv.style.display = "none";
@@ -17,7 +29,6 @@ function criteriaLength() {
 }
 
 function validateLength() {
-  let inputLength = document.querySelector("#input-length");
   if(inputLength.value < 8) {
     inputLength.value = 8;
   }
@@ -28,16 +39,78 @@ function validateLength() {
   passwordLength = Number(inputLength.value);
 }
 
-function generatePassword() {
+function noBoxesChecked() {
+  return (
+    !checkboxLength.checked &&
+    !checkboxLowercase.checked &&
+    !checkboxUppercase.checked &&
+    !checkboxNumeric.checked &&
+    !checkboxSpecial.checked
+  );
+}
 
+function generatePassword() {
+  // If none of the boxes are checked, simply return "password"
+  if(noBoxesChecked()) {
+    return "password";
+  }
+  
+  let password = "";
+  let numberOfCriteria = [];
+
+  if(checkboxLowercase.checked)
+    numberOfCriteria++;
+
+  if(checkboxUppercase.checked)
+    numberOfCriteria++;
+
+  if(checkboxNumeric.checked)
+    numberOfCriteria++;
+
+  if(checkboxSpecial.checked)
+    numberOfCriteria++;
+
+  for(let i = 0; i < passwordLength; i++) {
+    // Generate random number to randomly choose criteria
+    let randomCriteria = Math.floor(Math.random() * numberOfCriteria);
+
+    console.log(randomCriteria);
+
+    if(randomCriteria === 0 && checkboxLowercase.checked) {
+      password = password.concat(lowercaseLetters[Math.floor(Math.random() * 26)]);
+    }
+    else if(randomCriteria === 1 && checkboxUppercase.checked) {
+      password = password.concat(uppercaseLetters[Math.floor(Math.random() * 26)]);
+    }
+    else if(randomCriteria === 2 && checkboxNumeric.checked) {
+      password = password.concat(Math.floor(Math.random() * 10));
+    }
+    else if(randomCriteria === 3 && checkboxSpecial.checked) {
+      password = password.concat(specialCharacters[Math.floor(Math.random() * specialCharacters.length)]);
+    }
+  }
+
+  return password;
 }
 
 // Write password to the #password input
 function writePassword() {
+  inputLength.value = passwordLength;
   document.querySelector("#password").value = generatePassword();
 }
 
-// Add event listener to generate button
+// Add event listeners
 generateBtn.addEventListener("click", writePassword);
-inputLengthSet.addEventListener("click", validateLength)
-checkboxLength.addEventListener("click", criteriaLength)
+inputLengthSet.addEventListener("click", validateLength);
+
+checkboxLength.addEventListener("click", criteriaLength);
+checkboxLowercase.addEventListener("click", () => {
+  if(checkboxLength.checked && !checkboxLowercase.checked) {
+    checkboxUppercase.checked = true;
+  }
+});
+checkboxUppercase.addEventListener("click", () => {
+  if(checkboxLength.checked && !checkboxUppercase.checked) {
+    checkboxLowercase.checked = true;
+  }
+});
